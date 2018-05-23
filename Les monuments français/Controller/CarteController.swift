8fc894b20aeea5c22dd15bb12be986a0156360ea
rkorimba <korimbacus.riad@gmail.com>
2018-05-23 12:12:16 +0200

    UISegmentedControl

--- Les monuments français/Controller/CarteController.swift
+++ Les monuments français/Controller/CarteController.swift
@@ -7,29 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class CarteController: UIViewController {
 
+    @IBOutlet weak var carte: MKMapView!
+    @IBOutlet weak var maPositionBouton: UIButton!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func meLocaliser(_ sender: Any) {
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func segmentChoisi(_ sender: Any) {
+        switch segment.selectedSegmentIndex {
+        case 0: carte.mapType = .standard
+        case 1: carte.mapType = .satellite
+        case 2: carte.mapType = .hybrid
+        default: break
+        }
     }
-    */
 
 }
