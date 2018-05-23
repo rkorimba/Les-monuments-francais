9c2d8c669cc2a49e43b5787453f7452bd1b73bb1
rkorimba <korimbacus.riad@gmail.com>
2018-05-23 12:04:29 +0200

    Mise en place UI

---
+++ Les monuments français/Controller/CarteController.swift
@@ -0,0 +1,35 @@
+//
+//  CarteController.swift
+//  Les monuments français
+//
+//  Created by Riad Korimbacus on 23/05/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+class CarteController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
